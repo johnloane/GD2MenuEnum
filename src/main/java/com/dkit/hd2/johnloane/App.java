50e37a032ced2a50f80afc2deb2172bad6dd21c4
John Loane <john.loane@dkit.ie>
2020-11-19 11:17:53 +0000

    Getting started with the MenuEnum
    Showing GD2 how to use Enums to create a menu

---
+++ src/main/java/com/dkit/hd2/johnloane/App.java
@@ -0,0 +1,13 @@
+package com.dkit.hd2.johnloane;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/dkit/hd2/johnloane/MenuOptions.java
@@ -0,0 +1,6 @@
+package com.dkit.hd2.johnloane;
+
+public enum MenuOptions
+{
+    CONTINUE, CREATE_BANK, ADD_BRANCH, ADD_CUSTOMER, MAKE_TRANSACTION, DISPLAY_INFORMATION, QUIT;
+}
